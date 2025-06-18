8bb1a9adc85b815ad2f75b1ea505acee8061586a
Juan Pina <juanpinaribeiro@outlook.com>
2025-06-18 14:50:34 -0300

    validando cookies no fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -47,11 +47,21 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
         const { title, amount, type } = createTransactionBodySchema.parse(request.body)
 
+        let sessionId = request.cookies.sessionId
+
+        if (!sessionId) {
+            sessionId = randomUUID()
+            reply.cookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 7, // 7 days
+            })
+        }
+
         await knex('transactions').insert({
             id: randomUUID(),
             title,
             amount: type === 'credit' ? amount : amount * -1,
-
+            session_id: sessionId,
         })
 
         return reply.status(201).send()
