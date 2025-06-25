13406cf42c03e45b230f5945e69b68d61615ae44
Juan Pina <juanpinaribeiro@outlook.com>
2025-06-25 14:19:14 -0300

    test: categorizando os testes

---
+++ test/transactions.test.ts
@@ -0,0 +1,25 @@
+import { expect, test, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+    
+    afterAll(async () => {
+        await app.close()
+    })
+    
+    test('o usuário consegue criar uma nova transação', async () => {
+        
+        const response = await request(app.server)
+        .post('/transactions')
+        .send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit',
+        }).expect(201)
+    
+    })
+})
